a31ddda397afec709b4dca02438aeea8c8d51bf4
chenlanqing <743633145@qq.com>
2024-11-03 13:19:01 +0800

    orm demo

---
+++ orm/mybatis-demo/src/main/java/com/qing/fan/mybatis/MybatisApplication.java
@@ -0,0 +1,17 @@
+package com.qing.fan.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author QingFan
+ * @version 1.0.0
+ * @date 2024年11月03日 13:14
+ */
+@SpringBootApplication
+public class MybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}
---
+++ orm/mybatis-demo/src/main/java/com/qing/fan/mybatis/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author QingFan
+ * @version 1.0.0
+ * @date 2024年11月03日 13:09
+ */
+package com.qing.fan.mybatis;
